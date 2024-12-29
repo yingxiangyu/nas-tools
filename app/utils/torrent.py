3e35b3db583d87b0db9959d58c479e3d317cf60b
yuyx <yingxiang.yu.cn@gmail.com>
2024-12-29 10:33:34 +0800

    fix

--- app/utils/torrent.py
+++ app/utils/torrent.py
@@ -1,18 +1,19 @@
 import datetime
+import hashlib
 import os.path
-import time
 import re
 import tempfile
-import hashlib
-from urllib.parse import quote, unquote, urlencode, urlparse
+from urllib.parse import unquote
+from urllib.parse import urlencode
+from urllib.parse import urlparse
 
-import libtorrent
-from bencode import bencode, bdecode
+from bencode import bdecode
+from bencode import bencode
 
 import log
 from app.utils import StringUtils
-from app.utils.mteam_utils import MteamUtils
 from app.utils.http_utils import RequestUtils
+from app.utils.mteam_utils import MteamUtils
 from app.utils.types import MediaType
 from config import Config
 
@@ -292,49 +293,6 @@ class Torrent:
                 can_download_list.append(media_name)
                 can_download_list_item.append(t_item)
         return can_download_list_item
-
-    @staticmethod
-    def magent2torrent(url, path, timeout=20):
-        """
-        磁力链接转种子文件
-        :param url: 磁力链接
-        :param path: 保存目录
-        :param timeout: 获取元数据超时时间
-        :return: 转换后种子路径
-        """
-
-        log.info(f"【Downloader】转换磁力链接：{url}")
-        session = libtorrent.session()
-        magnet_info = libtorrent.parse_magnet_uri(url)
-        magnet_info.save_path = path
-        handle = session.add_torrent(magnet_info)
-
-        log.debug("【Downloader】获取元数据中")
-        tout = 0
-        while not handle.status().name:
-            time.sleep(1)
-            tout += 1
-            if tout > timeout:
-                log.debug("【Downloader】元数据获取超时")
-                return None, "种子元数据获取超时"
-        session.pause()
-
-        log.debug("【Downloader】获取元数据完成")
-        tf = handle.torrent_file()
-        ti = libtorrent.torrent_info(tf)
-        torrent_file = libtorrent.create_torrent(ti)
-        torrent_file.set_comment(ti.comment())
-        torrent_file.set_creator(ti.creator())
-
-        file_path = os.path.join(path, "%s.torrent" % handle.status().name)
-
-        with open(file_path, 'wb') as f_handle:
-            f_handle.write(libtorrent.bencode(torrent_file.generate()))
-            f_handle.close()
-
-        session.remove_torrent(handle, 1)
-        log.info(f"【Downloader】转换后的种子路径：{file_path}")
-        return file_path, ""
 
     @staticmethod
     def _write_binary_to_temp_file(binary_data):
